8b80d15da3115b932d4e740f989d0f4ccdd05e34
cmears89 <cmears1298@gmail.com>
2026-05-30 18:02:27 +0200

    Update table.py

--- table.py
+++ table.py
@@ -6,10 +6,6 @@ url="http://192.168.57.68:8080/table/"
 r=requests.get(url)
 soup=BeautifulSoup(r.text,"html.parser")
 
-flag=""
-
-for row in soup.find_all("tr")[1:]:
- cells=[c.get_text(strip=True) for c in row.find_all("td")]
- flag+="".join(cells)
-
-print(flag)
+for row in soup.find_all("tr"):
+ cells=[c.get_text(strip=True) for c in row.find_all(["td","th"])]
+ print(cells)
