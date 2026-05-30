85aff3d3f2717f75ab72e91ba93d6dc1cc189dc6
cmears89 <cmears1298@gmail.com>
2026-05-30 18:27:39 +0200

    Update post.py

--- post.py
+++ post.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 import requests
+from bs4 import BeautifulSoup
 
 base="http://192.168.57.68:8080/bijection"
 flag=""
 
 for i in range(100):
  r=requests.post(base+"?index="+str(i))
- c=r.text.strip()
+ text=BeautifulSoup(r.text,"html.parser").get_text("",strip=True)
 
- print(i, repr(c))
+ print(i, repr(text))
 
- if c == "" or "error" in c.lower():
+ if text == "" or "error" in text.lower():
   break
 
- flag += c
+ flag += text
 
- if c == "}":
+ if text == "}":
   break
 
 print("FLAG:",flag)
